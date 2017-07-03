fa9b20b14c0bbd7496bda349c1dca9f3357fc803
Aloving <Ilegend2011@gmail.com>
2017-07-03 17:57:00 +0300

    api/days/index fix small bug with init f

--- server/routes/api/days/indexPage.js
+++ server/routes/api/days/indexPage.js
@@ -4,5 +4,5 @@ const DayModel = require(`${__base}/models/Day`).Day;
 module.exports = function(req, res, next) {
   const getDays = () => DayModel.find({}).sort({ date: -1 }).limit(7);
 
-  getDays.then(markDaysItem).then(days => res.json(days)).catch(next);
+  getDays().then(markDaysItem).then(days => res.json(days)).catch(next);
 };

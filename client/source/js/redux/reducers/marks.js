50d47ef163c41df4a0ceea05d2e0c835e3ae7386
margarita <margarita@intervolga.ru>
2017-07-21 13:41:12 +0300

    adding default marks

--- client/source/js/redux/reducers/marks.js
+++ client/source/js/redux/reducers/marks.js
@@ -1,28 +1,37 @@
 export default function reducer(state = {}, action) {
-    switch (action.type) {
-        case 'GET_MARKS_START':
-            return action.payload;
-        case 'GET_MARKS_FINISH':
-            return {...action.data};
-        case 'ADD_MARK_START':
-            return state;
-        case 'ADD_MARK_ERROR' :
-            console.log(action.error);
-            return state;
-        case 'ADD_MARK_FINISH' :
-        console.log('REDUCER', action.data)
-          
-           return {
-            ...state, 
-            unDefaults: [
+  switch (action.type) {
+    case 'GET_MARKS_START':
+      return action.payload;
+    case 'GET_MARKS_FINISH':
+      return {...action.data};
+    case 'ADD_MARK_START':
+      return state;
+    case 'ADD_MARK_ERROR' :
+      console.log(action.error);
+      return state;
+    case 'ADD_MARK_FINISH' :
+      console.log('REDUCER', action.data)
+      var res = {}
+      action.data.defaultItem ?
+        res = {
+          ...state,
+          defaults: [
+            ...state.defaults,
+            action.data
+          ]
+        }
+        :
+        res = {
+          ...state,
+          unDefaults: [
             ...state.unDefaults,
-                 action.data
-            ]
-        
+            action.data
+          ]
+
         };
+      return res;
 
-
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 }

da7c80bb35d1c43c971b459e321a2777b177b7d6
akohas <doctorfreud33@gmail.com>
2017-08-08 13:06:29 +0300

    test data

--- server/scripts/startDb/startDaysData.js
+++ server/scripts/startDb/startDaysData.js
@@ -1,211 +1,702 @@
 module.exports = [
   {
-    date: '19.07.2017',
+    date: '08.08.2017',
     items: [
       {
-        title: 'Обед(Алёша)',
+        title: 'Обед',
         price: 300
-      },
-      {
-        title: 'Обед(Рита)',
-        price: 270
       },
       {
         title: 'Продукты',
         price: 857
       },
       {
-        title: 'Бухло',
-        price: 1000
+        title: 'Проезд',
+        price: 200
       }
     ]
   },
   {
-    date: '09.06.2017',
+    date: '07.08.2017',
     items: [
       {
-        title: 'Обед(Алёша)',
+        title: 'Обед',
         price: 300
       },
       {
-        title: 'Обед(Рита)',
-        price: 270
+        title: 'Проезд',
+        price: 200
+      }
+    ]
+  },
+  {
+    date: '06.08.2017',
+    items: [
+      {
+       title: 'Услуги сантехника',
+       price: 3000
+      },
+      {
+        title: 'Продукты',
+        price: 1857
+      }, 
+    ]
+  },
+   {
+    date: '05.08.2017',
+    items: [
+      {
+       title: 'Кино',
+       price: 1200
+      },
+      {
+        title: 'Кафе',
+        price: 3400
+      },
+       {
+        title: 'Одежда',
+        price: 10300
+      }
+    ]
+  },
+   {
+    date: '05.08.2017',
+     items:[ 
+     {
+        title: 'Обед',
+        price: 300
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+       {
+        title: 'Продукты',
+        price: 200
+      }
+    ]
+  },
+ {
+    date: '04.08.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 390
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      }, 
+    ]
+  },
+   {
+    date: '03.08.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 390
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '02.08.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+   {
+    date: '01.08.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+    {
+    date: '31.07.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+{
+    date: '30.07.2017',
+     items: [
+     {
+        title: 'День рождения',
+        price: 10000
+      },
+      {
+        title: 'Проезд',
+        price: 400
+      },
+      {
+        title: 'Продукты',
+        price: 1500
+    }
+    
+    ]
+  },
+  {
+    date: '29.07.2017',
+     items: [
+     {
+        title: 'Квартплата',
+        price: 30000
+      },
+      {
+        title: 'Одежда',
+        price: 4000
+      },
+      {
+        title: 'Проезд',
+        price: 200
+    }
+    
+    ]
+  },
+   {
+    date: '28.07.2017',
+     items: [
+     {
+        title: 'Гигиена',
+        price: 1000
+      },
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+  {
+    date: '28.07.2017',
+     items: [
+     {
+        title: 'Мобильная связь',
+        price: 500
+      },
+      {
+        title: 'Интернет',
+        price: 600
+      },
+
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+   {
+    date: '27.05.2017',
+     items: [
+
+      {
+        title: 'Телевизор',
+        price: 34000
+      },
+
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+  {
+    date: '27.07.2017',
+    items: [
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+   {
+    date: '26.07.2017',
+    items: [
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+   {
+    date: '25.07.2017',
+    items: [
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+   {
+    date: '17.07.2017',
+    items: [
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+   {
+    date: '16.07.2017',
+    items: [
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+   {
+    date: '14.07.2017',
+    items: [
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+   {
+    date: '11.07.2017',
+    items: [
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+   {
+    date: '01.07.2017',
+    items: [
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+   {
+    date: '04.07.2017',
+    items: [
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+   {
+    date: '30.06.2017',
+    items: [
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+   {
+    date: '17.06.2017',
+    items: [
+      {
+        title: 'Проезд',
+        price: 200
+    },
+     {
+        title: 'Обед',
+        price: 200
+    }
+    
+    ]
+  },
+
+
+  {
+    date: '08.06.2017',
+    items: [
+      {
+        title: 'Обед',
+        price: 300
       },
       {
         title: 'Продукты',
         price: 857
       },
       {
-        title: 'Бухло',
-        price: 1000
+        title: 'Проезд',
+        price: 200
       }
     ]
   },
-
-	{
-		date: '19.05.2017',
-		items: [
-			{
-				title: 'Обед(Алёша)',
-				price: 300
-			},
-			{
-				title: 'Обед(Рита)',
-				price: 270
-			},
-			{
-				title: 'Продукты',
-				price: 857
-			},
-			{
-				title: 'Бухло',
-				price: 1000
-			}
-		]
-	},
-	{
-		date: '18.05.2017',
-		items: [
-			{
-				title: 'Продукты',
-				price: 300
-			}
-		]
-	},
-	{
-		date: '16.05.2017',
-		items: [
-			{
-				title: 'Продукты',
-				price: 500
-			}
-		]
-	},
-	{
-		date: '15.05.2017',
-		items: [
-			{
-				title: 'Продукты',
-				price: 800
-			}
-		]
-	},
-
-    {
-        date: '13.05.2017',
-        items: [
-            {
-                title: 'Обед(Алёша)',
-                price: 300
-            },
-            {
-                title: 'Обед(Рита)',
-                price: 270
-            },
-            {
-                title: 'Продукты',
-                price: 857
-            },
-            {
-                title: 'Бухло',
-                price: 1000
-            }
-        ]
-    },
-    {
-        date: '12.05.2017',
-        items: [
-            {
-                title: 'Продукты',
-                price: 300
-            }
-        ]
-    },
-    {
-        date: '11.05.2017',
-        items: [
-            {
-                title: 'Продукты',
-                price: 500
-            }
-        ]
-    },
-    {
-        date: '10.05.2017',
-        items: [
-            {
-                title: 'Продукты',
-                price: 800
-            }
-        ]
-    },
-   
+  {
+    date: '07.06.2017',
+    items: [
+      {
+        title: 'Обед',
+        price: 300
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      }
+    ]
+  },
+  {
+    date: '06.06.2017',
+    items: [
+      {
+       title: 'Услуги сантехника',
+       price: 3000
+      },
+      {
+        title: 'Продукты',
+        price: 1857
+      }, 
+    ]
+  },
+   {
+    date: '05.06.2017',
+    items: [
+      {
+       title: 'Кино',
+       price: 1200
+      },
+      {
+        title: 'Кафе',
+        price: 3400
+      },
+       {
+        title: 'Одежда',
+        price: 10300
+      }
+    ]
+  },
+   {
+    date: '05.05.2017',
+     items:[ 
      {
-        date: '12.04.2017',
-        items: [
-            {
-                title: 'Работа',
-                price: 200
-            },
-            {
-                title: 'Обед(Алёша)',
-                price: 300
-            },
-            {
-                title: 'Бухло',
-                price: 1000
-            },
-            {
-                title: 'Бухло',
-                price: 300
-            },
-            {
-                title: 'Продукты',
-                price: 1200
-            },
-            {
-                title: 'Леваяеда',
-                price: 290
-            },
-            {
-                title: 'Шампунь',
-                price: 1000
-            },
-            {
-                title: 'Продукты',
-                price: 700
-            },
-            {
-                title: 'Продукты',
-                price: 200
-            },
-            {
-                title: 'Бухло',
-                price: 300
-            },
-            {
-                title: 'Бухло',
-                price: 300
-            },
-
-        ]
-    },
-    {
-        date: '10.09.2016',
-        items: [
-            {
-                title: 'Продукты',
-                price: 800
-            }
-        ]
-    },
-    {
-        date: '10.01.2016',
-        items: [
-            {
-                title: 'Продукты',
-                price: 800
-            }
-        ]
-    },
+        title: 'Обед',
+        price: 300
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+       {
+        title: 'Продукты',
+        price: 200
+      }
+    ]
+  },
+ {
+    date: '24.05.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 390
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      }, 
+    ]
+  },
+   {
+    date: '13.05.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 390
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '12.05.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+   {
+    date: '01.04.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '03.04.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '15.04.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '10.04.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '01.03.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '01.02.2017',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '01.12.2016',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '20.12.2016',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '15.12.2016',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '01.10.2016',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '01.09.2016',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '01.04.2016',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  {
+    date: '09.04.2016',
+     items: [
+     {
+        title: 'Обед',
+        price: 290
+      },
+      {
+        title: 'Проезд',
+        price: 200
+      },
+    
+    ]
+  },
+  
 ];
--- server/scripts/startDb/startMarks.js
+++ server/scripts/startDb/startMarks.js
@@ -1,32 +1,66 @@
 module.exports = [
     {
-        title: 'Обед(Алёша)',
+        title: 'Обед',
         defaultItem: true,
         create: '10.04.2017'
     },
     {
-        title: 'Обед(Рита)',
+        title: 'Кино',
+        defaultItem: false,
+        create: '10.04.2017'
+    },
+    {
+        title: 'Проезд',
         defaultItem: true,
+        create: '10.04.2017'
+    },
+    {
+        title: 'Услуги сантехника',
+        defaultItem: false,
+        create: '10.04.2017'
+    },
+    {
+        title: 'Телевизор',
+        defaultItem: false,
+        create: '10.04.2017'
+    },
+    {
+        title: 'Интернет',
+        defaultItem: true,
+        create: '10.04.2017'
+    },
+    {
+        title: 'Мобильная связь',
+        defaultItem: true,
+        create: '10.04.2017'
+    },
+    {
+        title: 'Одежда',
+        defaultItem: true,
+        create: '10.04.2017'
+    },
+    {
+        title: 'День рождения',
+        defaultItem: false,
         create: '10.04.2017'
     },
     {
         title: 'Продукты',
         defaultItem: true,
         create: '10.04.2017'
-    }, {
-        title: 'Бухло',
-        defaultItem: true,
-        create: '10.04.2017'
-    }, {
+    }, 
+    {
         title: 'Квартплата',
         defaultItem: true,
         create: '10.04.2017'
-    }, {
-        title: 'Леваяеда',
-        defaultItem: true,
+    }, 
+    {
+        title: 'Кафе',
+        defaultItem: false,
         create: '10.04.2017'
-    }, {
-        title: 'Шампунь',
+    }, 
+    {
+        title: 'Гигиена',
         defaultItem: false,
         create: '10.04.2017'
     }
